2c35591a002febd3676619e9d6b01eb121d7e1a8
Gustavo Nunes <gustavo_nw@outlook.com>
2024-02-08 15:38:14 -0300

    Criação da classe Estudante para que extendesse a classe Pessoa

--- Estudante.js
+++ Estudante.js
@@ -1,1 +1,11 @@
-class Estudante{}
+class Estudante extends Pessoa {
+
+    constructor(nome, anoNascimento, profissao, matricula) {
+        super(nome, anoNascimento, profissao);
+        this.matricula = matricula;
+    }
+}
+
+const aluno1 = new Estudante("Jorge", 2001, "Dev", 202020);
+
+console.log(aluno1);
