9c601bc49513499e708d319918dd52f92c6f487d
Gustavo Nunes Wawrick <gustavo.wawrick@itsgroup.com.br>
2024-02-07 21:44:27 -0300

    Criação de Objeto, Função e alteração de valores de variavéis

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log("Olá, mundo!");
+var pessoa = {
+    nome: "Gustavo", idade: 21, saudar: function () {
+        console.log('Olá');
+    }
+};
+
+var pessoa1 = {
+    nome: "Brenda", idade: 19, saudar: function () {
+        console.log("Oii");
+    }
+};
+
+pessoa1.nome = "Valéria";
+
+console.log(pessoa, pessoa.saudar);
+console.log(pessoa1, pessoa.saudar);
