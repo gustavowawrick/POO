3f5e19e59415162f376ecc5387057764cae0026f
Gustavo Nunes <gustavo_nw@outlook.com>
2024-02-09 10:06:28 -0300

    Arquivo deletado

--- LogicaPOO/const3C.js
+++
@@ -1,9 +0,0 @@
-function testConst() {
-    const const01 = { propriedade: "Valor" }
-    console.log("01A." + const01.propriedade);
-
-    const01 = { propriedade: "Novo Valor"}
-    console.log("02A." + const01.propriedade);
-}
-
-testConst();
