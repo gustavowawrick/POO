439e09c83d4caa2f3c9e8f00125b26e4116ef9ff
Gustavo Nunes <gustavo_nw@outlook.com>
2024-02-12 10:55:41 -0300

    Criação dos arquivos consumidor e definicao utilizando o ES6 para realizar exportacoes e importacoes

---
+++ LogicaPOO/modulo/consumidorES603A.mjs
@@ -0,0 +1,8 @@
+import { area, circunferencia as circ } from "./definicaoES603A.mjs";
+
+console.log(`Área do círculo de raio 4: ${area(4)}`);
+console.log(`Circunferência do círculo de raio 4: ${circ(4)}`);
+
+import * as circulo from "./definicaoES603A.mjs";
+
+console.log(`Área do círculo de raio 2: ${circulo.area(2)}`);
---
+++ LogicaPOO/modulo/consumidorES604A.mjs
@@ -0,0 +1,5 @@
+import { Circulo } from "./definicaoES604A.mjs";
+
+const c1 = new Circulo(4);
+
+console.log(`Área do círculo de raio 4: ${c1.area()}`);
