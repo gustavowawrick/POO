6a81952defde608d52149b58b02eb6baa6ef852e
Gustavo Nunes <gustavo_nw@outlook.com>
2024-02-13 23:36:41 -0300

    Adiciona exemplo de conversão de objeto para JSON em JavaScript
    Este commit adiciona um exemplo de código que converte um objeto JavaScript para uma string JSON usando o método JSON.stringify().

---
+++ LogicaPOO/json/objetoJson.js
@@ -0,0 +1,10 @@
+let student = {
+    name: 'Gustavo',
+    age: 21,
+    isAdmin: false,
+    courses: ['html', 'css', 'js'],
+    wife: null
+};
+
+let json = JSON.stringify(student);
+console.log(json);
