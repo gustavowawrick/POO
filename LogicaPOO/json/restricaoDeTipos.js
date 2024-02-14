f85e9a410655ebf049e12e376467dd4679add03e
Gustavo Nunes <gustavo_nw@outlook.com>
2024-02-13 23:44:20 -0300

    Adiciona exemplo de conversão de JSON para objeto com manipulação de dados em JavaScript
    Este commit adiciona um exemplo de código que converte uma string JSON para um objeto JavaScript usando o método JSON.parse(), com uma função de reviver para manipular os dados durante o processo de parsing. Neste exemplo, a chave 'birth' é transformada em um objeto Date.

---
+++ LogicaPOO/json/restricaoDeTipos.js
@@ -0,0 +1,11 @@
+var json = '{ "name":"Gustavo Nunes", "birth":"2002-03-14T12:00:00.000Z", "city":"Canoas"}';
+
+var obj = JSON.parse(json, function (key, value) {
+    if (key == "birth") {
+        return new Date(value);
+    }
+    return value;
+});
+
+console.log("Nome:  " + obj.name);
+console.log("Data de nascimento: " + obj.birth);
