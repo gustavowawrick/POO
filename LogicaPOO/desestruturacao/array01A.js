f0aa33181dadf9202bea36022f2b5cc8e8ce881e
Gustavo Nunes <gustavo_nw@outlook.com>
2024-02-13 23:34:14 -0300

    Inserido o ; nos elementos do arquivo

--- LogicaPOO/desestruturacao/array01A.js
+++ LogicaPOO/desestruturacao/array01A.js
@@ -1,14 +1,14 @@
 let nomeCompleto = ['Gustavo', "Nunes", "Warick"];
 var [primeiroNome, nomeDoMeio, ultimoNome] = nomeCompleto;
 
-console.log("O primeiro nome é: " + primeiroNome)
-console.log("O nome do meio é: " + nomeDoMeio)
-console.log("O último nome é: " + ultimoNome)
+console.log("O primeiro nome é: " + primeiroNome);
+console.log("O nome do meio é: " + nomeDoMeio);
+console.log("O último nome é: " + ultimoNome);
 
-var [primeiroNome, ...restante] = nomeCompleto
-console.log("O primeiro nome é: " + primeiroNome)
-console.log("O restante do nome é: \"" + restante.toString().replace(',', ' ') + '"')
+var [primeiroNome, ...restante] = nomeCompleto;
+console.log("O primeiro nome é: " + primeiroNome);
+console.log("O restante do nome é: \"" + restante.toString().replace(',', ' ') + '"');
 
-var [soOPrimeiroNome, , soOUltimoNome] = nomeCompleto
-console.log("O primeiro nome é: " + soOPrimeiroNome)
-console.log("O último nome é: " + soOUltimoNome)
+var [soOPrimeiroNome, , soOUltimoNome] = nomeCompleto;
+console.log("O primeiro nome é: " + soOPrimeiroNome);
+console.log("O último nome é: " + soOUltimoNome);
